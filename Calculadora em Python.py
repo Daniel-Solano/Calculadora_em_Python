855b5bfb9f3268a8209d7e9b793019de049c9f20
Daniel-Solano <danielsolano1000@gmail.com>
2020-03-10 21:36:38 -0300

    Primeiro commit

---
+++ Calculadora em Python.py
@@ -0,0 +1,39 @@
+# Calculadora em Python
+
+oper = int(input('Selecione o número da opção desejada: \n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\nDigite sua opção (1/2/3/4): '))
+
+def erro():
+    print('Opção inválida')
+
+if oper == 1 or oper == 2 or oper == 3 or oper == 4:
+
+    x = input('Digite o primeiro número: ')
+    y = input('Digite o segundo número: ')
+
+    def soma(x,y):
+        total = float(x) + float(y)
+        print(x, ' + ', y, ' = ', total)
+
+    def subt(x,y):
+        total = float(x) - float(y)
+        print(x, ' - ', y, ' = ', total)
+
+    def mult(x,y):
+        total = float(x) * float(y)
+        print(x, ' * ', y, ' = ', total)
+
+    def div(x,y):
+        total = float(x) / float(y)
+        print(x, ' - ', y, ' = ', round(total, 2))
+
+    if oper == 1:
+        soma(x,y)
+    elif oper == 2:
+        subt(x,y)
+    elif oper == 3:
+        mult(x,y)
+    elif oper == 4:
+        div(x,y)
+          
+else:
+    erro() 
